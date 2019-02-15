50110f8071ba8b5c6a89f687d07b0c765a08b33e
Zarah Dominguez <zarah@zdominguez.com>
2019-02-15 15:03:11 +1100

    Add check on Action ID

--- app/src/main/java/com/zdominguez/sdksandbox/DataBindingImportBug.kt
+++ app/src/main/java/com/zdominguez/sdksandbox/DataBindingImportBug.kt
@@ -1,6 +1,9 @@
 package com.zdominguez.sdksandbox
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
@@ -14,8 +17,13 @@ class DataBindingImportBug : AppCompatActivity() {
         binding.handlers = this
     }
 
-    fun onEditorAction() : Boolean {
-        Toast.makeText(this, "Action clicked!", Toast.LENGTH_LONG).show()
+    fun onEditorAction(view: TextView, actionId: Int?, event: KeyEvent?) : Boolean {
+        var message = "Action clicked!"
+        if (actionId == EditorInfo.IME_ACTION_SEND) {
+            message = "Send was clicked!"
+        }
+        Toast.makeText(this, message, Toast.LENGTH_LONG)
+            .show()
         return false
     }
 }
