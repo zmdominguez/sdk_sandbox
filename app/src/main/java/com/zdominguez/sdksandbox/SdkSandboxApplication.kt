3c63a05513e89a4ef3b350c45868fbf91705c7e8
Zarah Dominguez <zarah@zdominguez.com>
2019-06-09 18:33:34 +1000

    Add Timber dependency.

---
+++ app/src/main/java/com/zdominguez/sdksandbox/SdkSandboxApplication.kt
@@ -0,0 +1,16 @@
+package com.zdominguez.sdksandbox
+
+import android.app.Application
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+
+class SdkSandboxApplication : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        }
+    }
+}
