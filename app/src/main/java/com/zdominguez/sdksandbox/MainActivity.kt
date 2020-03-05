1b4cc0d10c2918f4da5768964d595dfa3dc34317
Zarah Dominguez <zarah@zarah.dev>
2020-03-05 16:49:28 +1100

    Remove deprecated notifications

--- app/src/main/java/com/zdominguez/sdksandbox/MainActivity.kt
+++ app/src/main/java/com/zdominguez/sdksandbox/MainActivity.kt
@@ -1,41 +1,24 @@
 package com.zdominguez.sdksandbox
 
-import android.app.Notification
-import android.app.NotificationChannel
-import android.app.NotificationManager
-import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import androidx.databinding.DataBindingUtil
-import android.graphics.BitmapFactory
-import android.graphics.Color
-import android.net.Uri
-import android.os.Build
 import android.os.Bundle
 import android.preference.PreferenceManager
-import androidx.core.app.NotificationCompat
-import androidx.core.content.ContextCompat
-import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
-import android.text.Html
 import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import butterknife.OnClick
 import com.zdominguez.sdksandbox.bottomsheet.BottomSheetShare
 import com.zdominguez.sdksandbox.databinding.ActivityMainBinding
 import com.zdominguez.sdksandbox.databinding.DialogDataBindingDemoBinding
 import com.zdominguez.sdksandbox.models.AdventureTimeCharacters
-
 import org.parceler.Parcels
-
-import java.util.ArrayList
-import java.util.Random
-
-import butterknife.OnClick
-import com.zdominguez.sdksandbox.R.string
 import java.io.File
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -113,140 +96,6 @@ class MainActivity : AppCompatActivity() {
             }
         }
         alert.show()
-    }
-
-    fun onSendNotificationClick() {
-        createNotificationChannel()
-
-        //sendInboxStyleNotification()
-
-        sendBagReminder()
-    }
-
-    private fun sendInboxStyleNotification() {
-        val builder = NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID)
-        val title = "This is a test notification"
-        builder
-            .setSmallIcon(R.drawable.ic_notification)
-            .setContentTitle(Html.fromHtml(title))
-            .setContentText(Html.fromHtml("<i>Wow</i> I am from HTML!"))
-            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-            .setTicker("Testing notifications!")
-            .setLights(Color.GREEN, 1000, 1000)
-            .setNumber(30)
-            .setGroupSummary(true)
-            .setAutoCancel(true)
-            .setCategory(NotificationCompat.CATEGORY_PROMO)
-        builder.setLocalOnly(true)
-        val style = NotificationCompat.InboxStyle()
-        val inboxLines = ArrayList<String>()
-        inboxLines.add("Line 1")
-        inboxLines.add("Line 2")
-        inboxLines.add("Line 3")
-        inboxLines.add("Line 4")
-        inboxLines.add("Line 5")
-        inboxLines.add("Line 6")
-        inboxLines.add("Line 7")
-        inboxLines.add("Line 8")
-        inboxLines.add("Line 9")
-        inboxLines.add("Line 10")
-        for (inboxLine in inboxLines) {
-            style.addLine(inboxLine)
-        }
-        style.setBigContentTitle(Html.fromHtml(title))
-            .setSummaryText("Tiny summary text!")
-        builder.setStyle(style)
-
-        val notificationIntent = Intent(this, MainActivity::class.java)
-        notificationIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-
-        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
-        builder.setContentIntent(pendingIntent)
-
-        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        nm.cancel(NOTIFICATION_TAG, NOTIFICATION_REQUEST_CODE)
-        nm.notify(NOTIFICATION_TAG, NOTIFICATION_REQUEST_CODE, builder.build())
-    }
-
-    private fun sendBagReminder() {
-        val context = this
-
-        val notificationIntent = Intent(this, MainActivity::class.java)
-        notificationIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
-
-        val bagReminderSetup = NotificationCompat.Builder(context, NOTIFICATION_CHANNEL_ID)
-            .setSmallIcon(R.drawable.ic_notification)
-            .setColor(ContextCompat.getColor(context, R.color.color_accent))
-            .setContentTitle("Never forget your bags")
-            .setContentText("We can help you to bring your reusable bags. Tap here to set up notifications today.")
-            .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.intro_2))
-            .setStyle(NotificationCompat.BigTextStyle()
-                .bigText("We can help you to bring your reusable bags. Tap here to set up notifications today."))
-            .setContentIntent(pendingIntent)
-            .setDefaults(Notification.DEFAULT_ALL)
-            .setAutoCancel(true)
-
-        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        nm.cancel(NOTIFICATION_TAG, NOTIFICATION_REQUEST_CODE)
-        nm.notify(NOTIFICATION_TAG, NOTIFICATION_REQUEST_CODE, bagReminderSetup.build())
-    }
-
-    val NOTIFICATION_CHANNEL_ID = "429017"
-
-    private fun createNotificationChannel() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel(NOTIFICATION_CHANNEL_ID, "Bag Reminder",
-                NotificationManager.IMPORTANCE_DEFAULT).apply {
-                enableLights(true)
-                enableVibration(true)
-
-                val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-                nm.createNotificationChannel(this)
-            }
-        }
-    }
-
-    fun onSendNotificationPeekClick() {
-        val builder = NotificationCompat.Builder(this)
-        val title = "Peeking notification"
-        builder
-            .setSmallIcon(R.drawable.ic_notification)
-            .setContentTitle(Html.fromHtml(title))
-            .setContentText(Html.fromHtml("<b>Homepass:</b> Welcome to your inspection!"))
-            .setPriority(NotificationCompat.PRIORITY_HIGH)
-            .setTicker("You are pre-registered for this inspection")
-            .setLights(Color.GREEN, 1000, 1000)
-            .setNumber(30)
-            .setGroupSummary(true)
-            .setAutoCancel(true)
-            .setCategory(NotificationCompat.CATEGORY_PROMO)
-        builder.setLocalOnly(true)
-
-        val alarmSound = PreferenceManager.getDefaultSharedPreferences(this)
-            .getString("notifications_new_message_ringtone", null)
-        builder.setSound(Uri.parse(alarmSound))
-
-        val style = NotificationCompat.InboxStyle()
-        val inboxLines = ArrayList<String>()
-        inboxLines.add("3/60 Warialda Street, Kogarah, NSW")
-        inboxLines.add("1 bed, 1 bath, 1 carspace")
-        for (inboxLine in inboxLines) {
-            style.addLine(inboxLine)
-        }
-        style.setBigContentTitle(Html.fromHtml(title))
-            .setSummaryText("Tiny summary text!")
-        builder.setStyle(style)
-
-        val notificationIntent = Intent(this, MainActivity::class.java)
-        notificationIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_SINGLE_TOP
-
-        val pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0)
-        builder.setContentIntent(pendingIntent)
-
-        val nm = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        nm.cancel(NOTIFICATION_TAG, NOTIFICATION_REQUEST_CODE)
-        nm.notify(NOTIFICATION_TAG, NOTIFICATION_REQUEST_CODE, builder.build())
     }
 
     fun onRecyclerViewViewStub() {
